ca027a9df11fddab379f86d755ae83a686938895
TT <2248648168@163.com>
2022-05-16 23:22:59 +0800

    add finish sentence

--- bikeshare.py
+++ bikeshare.py
@@ -255,7 +255,8 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+        else:
+            print('Thank you！')
 
 if __name__ == "__main__":
 
